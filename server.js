f1bc75082f44e891ca64505351e25a977568e5dc
Amithkumar <amithkumarsaiamithkumarabhinav@gmail.com>
2026-04-07 13:16:27 +0530

    Refactor server.js for code organization and clarity

--- server.js
+++ server.js
@@ -105,16 +105,6 @@ app.post("/tasks", auth, async (req, res) => {
     }
 });
 
-axios.post(API + "/tasks", {
-    title: taskInput.value,
-    category: category.value,
-    priority: priority.value,
-    dueDate: dueDate.value
-}, {
-    headers: {
-        Authorization: "Bearer " + localStorage.getItem("token")
-    }
-});
 
 // UPDATE TASK
 app.put("/tasks/:id", auth, async (req, res) => {
